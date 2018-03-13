48b98feee35bb4869e3ec4f205fa16d3b97b83f2
Rudresha, Gulaganjihalli Parameshappa <gulaganjihalliparameshappa@campus.tu-berlin.de>
2018-03-13 17:39:33 +0100

    Updated headers

--- ReadWriteThread/ReadThread.cs
+++ ReadWriteThread/ReadThread.cs
@@ -6,14 +6,14 @@ using Newtonsoft.Json;
 namespace WriteReadSameFileThreading.ReaWriteThred
 {
     //  Summary:
-    //  Watches file and any changes
+    //      Watches file for any changes
     //  Parameters:
     //      T:
     //          Type of the class or object need to be read
     public class ReadThread<T> where T : class
     {
         //  Summary:
-        ///     Queue to add read items
+        //      Queue to add read items
         private readonly BlockingCollection<T> _blockingCollection;
 
         //  Summary:
@@ -21,18 +21,18 @@ namespace WriteReadSameFileThreading.ReaWriteThred
         private readonly string _inputFileName;
 
         //  Summary:
-        ///     Folder in which file present else application path is choosen
+        //     Folder in which file present else application path is choosen
         private readonly string _fileFolder;
 
         //  Summary:
         //      Initialize a read thread and feed blocking collection
-        // Parameters:
-        //   blockingCollection:
-        //      The queue to which data need to be put
-        //   inputFileName:
-        //      name of the file from which data to be read
-        //   fileFolder:
-        //      folder which contains the file
+        //  Parameters:
+        //      blockingCollection:
+        //          The queue to which data need to be put
+        //      inputFileName:
+        //          name of the file from which data to be read
+        //      fileFolder:
+        //          folder which contains the file
         public ReadThread(BlockingCollection<T> blockingCollection, string inputFileName, string fileFolder = null)
         {
             _blockingCollection = blockingCollection;
@@ -45,7 +45,7 @@ namespace WriteReadSameFileThreading.ReaWriteThred
         //      <see href="https://stackoverflow.com/questions/3791103/c-sharp-continuously-read-file"/>
         public void ReadItem()   
         {
-            //Wait till file is avavilable TODO: probably better timeout strategy might be implemented
+            // Wait till file is avavilable TODO: probably better timeout strategy might be implemented
             while (!File.Exists(_inputFileName)) { }
             var waitHandler = new AutoResetEvent(false); // To handle when new data is written by the Write Thread
             using (var fileSystemWatcher = new FileSystemWatcher(_fileFolder, _inputFileName))
